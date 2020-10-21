0ef18cd07e508f420ee2453493827b9aef5294a6
JoramNL <nljoram@live.nl>
2020-10-21 22:07:22 +0200

    Identation fixes

--- src/commands/utility/userid.js
+++ src/commands/utility/userid.js
@@ -1,24 +1,24 @@
 const { Command } = require("discord.js-commando");
 
-module.exports = class PingCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'userid',
-			group: 'utility',
-			memberName: 'userid',
-            description: 'Returns the userid of you or the user you mentioned',
-            guildOnly: true
-		});
-	}
+module.exports = class UserIdCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'userid',
+      group: 'utility',
+      memberName: 'userid',
+      description: 'Returns the userid of you or the user you mentioned',
+      guildOnly: true
+    });
+  }
 
-	run(message) {
-        const mentionedClient = message.mentions.members.first();
+  run(message) {
+    const mentionedClient = message.mentions.members.first();
 
-		if (!mentionedClient) {
-			return message.say(`:1234: <@${message.member.id}>'s your userID is: ${message.member.id}`);
-		}
-		else {
-			return message.say(`:1234: <@${mentionedClient.id}>'s userID is: ${mentionedClient.id}`);
-        }
-	}
+    if (!mentionedClient) {
+      return message.reply(`:1234: your userID is: ${message.member.id}`);
+    }
+    else {
+      return message.say(`:1234: <@${mentionedClient.id}>'s userID is: ${mentionedClient.id}`);
+    }
+  }
 };
--- src/index.js
+++ src/index.js
@@ -5,26 +5,26 @@ const path = require('path');
 const config = require('./config.json');
 
 const client = new CommandoClient({
-	commandPrefix: config.prefix,
-	owner: config.owner,
+  commandPrefix: config.prefix,
+  owner: config.owner,
 });
 
 client.registry
-	.registerDefaultTypes()
-	.registerGroups([
-		['utility', 'Utility commands to make your life better.'],
-		['fun', 'Commands to have a laugh'],
-	])
-	.registerDefaultGroups()
-	.registerDefaultCommands({
-		ping: false,
-	})
-	.registerCommandsIn(path.join(__dirname, 'commands'));
+  .registerDefaultTypes()
+  .registerGroups([
+    ['utility', 'Utility commands to make your life better.'],
+    ['fun', 'Commands to have a laugh'],
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands({
+    ping: false,
+  })
+  .registerCommandsIn(path.join(__dirname, 'commands'));
 
 
 client.once('ready', () => {
-	console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-	client.user.setActivity(`${config.prefix}help | exteme-network.net`);
+  console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+  client.user.setActivity(`${config.prefix}help | exteme-network.net`);
 });
 
 client.on('error', console.error);
