bdf5fa9b72c4c60cc24e783e622cb5d4ff3b1ba9
JoramNL <nljoram@live.nl>
2020-10-21 20:54:21 +0200

    Setting up linter + Nodemon + Created config file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  client.user.setActivity(`${config.prefix}help | exteme-network.net`);
 });
 
 client.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === 'Who is a French bitch?') {
+    msg.reply('Bagout, of course');
   }
 });
 
